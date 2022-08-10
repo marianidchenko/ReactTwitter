9c7b4e23d86332b23fc0cffa22391dd01ff64479
Maria Nidchenko <marianidchenko@gmail.com>
2022-08-10 21:39:15 +0300

    added go to homepage link

--- src/components/Errors/NotFound.js
+++ src/components/Errors/NotFound.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
 import "./errors.css";
 
 export const NotFound = () => {
     return (
         <div className="warningWrapper">
-            <h1 className="warning">404</h1>
+            <h1 className="code">404</h1>
             <h1 className="warning">Page not found!</h1>
+            <Link to ="/" className="homeBtn">Go to homepage</Link>
         </div>
 
     )
